be9fbecaea5a2673dd41114d8d5f8919fd7768ca
Wendy <xccxccxccb57@gmail.com>
2024-02-06 23:36:11 +1100

    fix send prize issues

--- script/Game.s.sol
+++ script/Game.s.sol
@@ -9,8 +9,18 @@ contract GameScript is Script {
         vm.startBroadcast();
 
         address _minimalForwarder = 0xB6A87320DE35F2bEFE2258162360daa3de11C788;
+        address gameTicket = 0x5Ff7758B2d501f74018C0dbbF5Be47d6E1ADe18B;
+        address gameDeveloper = 0xAC259056C1A7C974AAb17908BC1d9914DF401249;
 
-        // Game game = new Game(3, "emojiMatch", 604800, 86400, _minimalForwarder);
+        Game game = new Game(
+            3,
+            "emojiMatch",
+            604800,
+            86400,
+            _minimalForwarder,
+            gameTicket,
+            gameDeveloper
+        );
 
         vm.stopBroadcast();
     }
